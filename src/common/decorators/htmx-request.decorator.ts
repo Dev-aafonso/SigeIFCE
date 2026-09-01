dedac554cede8fed8dbe4a7ed5cbf2deb23525b3
Dev-aafonso <afonsofilho692@gmail.com>
2026-09-01 08:09:00 -0300

    chore: reorganize project architecture

---
+++ src/common/decorators/htmx-request.decorator.ts
@@ -0,0 +1,17 @@
+﻿import {
+  createParamDecorator,
+  ExecutionContext,
+} from '@nestjs/common';
+
+export const HtmxRequest = createParamDecorator(
+  (
+    _data: unknown,
+    ctx: ExecutionContext,
+  ): boolean => {
+
+    const request =
+      ctx.switchToHttp().getRequest();
+
+    return request.headers['hx-request'] === 'true';
+  },
+);
---
+++ src/common/interceptors/htmx.interceptor.ts
@@ -0,0 +1,36 @@
+﻿import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HtmxInterceptor
+  implements NestInterceptor
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<any> {
+
+    const request =
+      context.switchToHttp().getRequest();
+
+    const response =
+      context.switchToHttp().getResponse();
+
+    const isHtmx =
+      request.headers['hx-request'] === 'true';
+
+    if (isHtmx) {
+      response.locals.isHtmx = true;
+    } else {
+      response.locals.isHtmx = false;
+    }
+
+    return next.handle();
+  }
+}
